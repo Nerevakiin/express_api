ff6e10a67a66adf29a769c833a4c4d0e0d6ca263
Nerevakiin <djmatisk@yahoo.gr>
2026-01-22 21:38:55 +0200

    added data

--- server.js
+++ server.js
@@ -4,4 +4,13 @@ const PORT = 8000
 
 const app = express()
 
+const celebrity = {
+  type: "action hero",
+  name: "JSON Statham"
+}
+
+app.get('/', (req,res) => { // having req first and res second apparently is important
+    res.json(celebrity)
+})
+
 app.listen(PORT, () => console.log(`server connected on port: ${PORT}`))
