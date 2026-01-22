a76ac805b989df129cc4226cb93693ff052f6ec9
Nerevakiin <djmatisk@yahoo.gr>
2026-01-23 00:03:14 +0200

    added logic that checks and validates the data

--- server.js
+++ server.js
@@ -1,16 +1,49 @@
 import express from 'express'
+import { startups } from './data/data.js'
 
 const PORT = 8000
 
 const app = express()
 
-const celebrity = {
-  type: "action hero",
-  name: "JSON Statham"
-}
 
 app.get('/', (req,res) => { // having req first and res second apparently is important
-    res.json(celebrity)
+    
+    let filteredData = startups
+
+    const { industry, country, continent, is_seeking_funding, has_mvp } = req.query
+
+    if (industry) {
+        filteredData = filteredData.filter(startup =>
+             startup.industry.toLowerCase() === industry.toLowerCase())
+    }
+
+    if (country) {
+        filteredData = filteredData.filter(startup =>
+             startup.country.toLowerCase() === country.toLowerCase())
+    }
+
+    if (continent) {
+        filteredData = filteredData.filter(startup =>
+             startup.continent.toLowerCase() === continent.toLowerCase())
+    }
+
+    
+    
+    // convert the strings to booleans
+    if (is_seeking_funding) {
+        filteredData = filteredData.filter(startup =>
+             startup.is_seeking_funding === JSON.parse(is_seeking_funding.toLowerCase())) // for these occasions, JSON.parse converts string to boolean
+    }
+
+    if (has_mvp) {
+        filteredData = filteredData.filter(startup =>
+             startup.has_mvp === JSON.parse(has_mvp.toLowerCase()))
+    }
+
+
+    
+    
+    res.json(filteredData)
 })
 
 app.listen(PORT, () => console.log(`server connected on port: ${PORT}`))
