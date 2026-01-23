cd627bd37aaf672a2c8b6b99cecf58e9c59c6fc7
Nerevakiin <djmatisk@yahoo.gr>
2026-01-23 12:42:17 +0200

    finished project
    also added CORS

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 import express from 'express'
 import { apiRouter } from './routes/apiRouter.js'
+import cors from 'cors'
 
 
 const PORT = 8000
 
 const app = express()
 
-app.use('/api', apiRouter)
+app.use(cors()) // CORS allow us to deploy our backend so others can also interact with it
 
+app.use('/api', apiRouter) // all the logic that handles the backend
+
+
+// fallback if user enters wrong/non supported url
 app.use((req, res) => {
     res.status(400).json({
         message: "endpoint not found"
